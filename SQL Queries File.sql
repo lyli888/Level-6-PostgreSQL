07b65b2290e2c847727d7ad350849c0cd27dd01e
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-10 18:32:27 -0500

    Create SQL Queries File.sql

---
+++ SQL Queries File.sql
@@ -0,0 +1,39 @@
+SQL Queries File *PostGres 11*
+
+1. List the following details of each employee: employee number, last name, first name, sex, and salary. 
+
+SELECT first_name, last_name, sex, employees.emp_no FROM Employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date FROM employees
+WHERE hire_date >= '1986-01-01' AND hire_date < '1987-01-01';
+
+3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+
+4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+
+5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+
+7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name. 
+
+
+
+8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)AS Frequency
+FROM employees
+ GROUP BY last_name
+ ORDER BY COUNT(last_name) DESC;
