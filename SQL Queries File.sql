69a17d90508b12a68ffab1bc6c57ee6bea90dc66
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-10 19:07:39 -0500

    Update SQL Queries File.sql

--- SQL Queries File.sql
+++ SQL Queries File.sql
@@ -2,7 +2,7 @@ SQL Queries File *PostGres 11*
 
 1. List the following details of each employee: employee number, last name, first name, sex, and salary. 
 
-SELECT first_name, last_name, sex, employees.emp_no FROM Employees
+SELECT first_name, last_name, sex, employees.emp_no FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 2. List first name, last name, and hire date for employees who were hired in 1986.
