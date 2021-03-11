0b151caf31eb39765e501379627a724120fa6ef7
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-10 20:13:46 -0500

    Update SQL Queries File.sql

--- SQL Queries File.sql
+++ SQL Queries File.sql
@@ -21,6 +21,13 @@ employees on department_manager.emp_no = employees.emp_no;
 
 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
+SELECT departments.dept_name,
+dept_emp.emp_no,
+employees.first_name, employees.last_name
+FROM dept_emp
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no;
 
 
 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
@@ -30,10 +37,21 @@ WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-
+SELECT departments.dept_name,
+       dept_emp.emp_no,
+	   employees.last_name,
+	   employees.first_name
+ FROM dept_emp
+ INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+ INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+ WHERE departments.dept_no = 'd007';
 
 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name. 
 
+SELECT departments.dept_name, dept_emp.emp_no, employees.first_name, employees.last_name
+FROM dept_emp  
+LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no   
+LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE departments.dept_no = 'd007' OR departments.dept_no = 'd005';
 
 
 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
